ef2def8468357b058bc6f33282a900aa399c1be3
Grzegorz Cagara <grzegorzcagara@gmail.com>
2022-04-04 22:17:36 +0200

    * add test results reporting

---
+++ src/test/resources/SpockConfig.groovy
@@ -0,0 +1,6 @@
+spockReports {
+    set 'com.athaydes.spockframework.report.showCodeBlocks': false
+    set 'com.athaydes.spockframework.report.outputDir': 'out/spock-reports'
+    set 'com.athaydes.spockframework.report.template.TemplateReportCreator.summaryFileName': 'index.html'
+
+}
